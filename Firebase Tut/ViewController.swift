ceb950e29f6847553969f0be55f64ba88b7d39c0
TechCoderX <techcoderx@techcoderxs-mac-pro.local>
2017-12-27 15:05:18 +0800

    Initial Commit

---
+++ Firebase Tut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase Tut
+//
+//  Created by TechCoderX on 27/12/2017.
+//  Copyright © 2017 TechCoderX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
